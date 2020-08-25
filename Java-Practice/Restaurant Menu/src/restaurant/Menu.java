30ad4bba579f0f79ad38c87dccb75ff1daabeb61
Nat Hilterbrand <nat.hilterbrand@gmail.com>
2020-08-24 20:25:18 -0500

    Java Class Three Studio

---
+++ Java-Practice/Restaurant Menu/src/restaurant/Menu.java
@@ -0,0 +1,36 @@
+package restaurant;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    private ArrayList<MenuItem> menuItems;
+    private Date dateUpdated;
+
+    public Menu(ArrayList<MenuItems> menuItems, Date dateUpdated){
+        this.menuItems = new ArrayList<>();
+        this.dateUpdated = dateUpdated;
+    }
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void setMenuItems(ArrayList<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
+
